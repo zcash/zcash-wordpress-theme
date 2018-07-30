2ba2ae7b5f0644599932500f293ff29ffce446b0
copernicus-mogley <6251098+copernicus-mogley@users.noreply.github.com>
2018-07-30 14:20:20 -0600

    Remove mailchimp signup

--- footer.php
+++ footer.php
@@ -81,46 +81,6 @@ Community						</li>
 					</ul>
 				</div>
 			</div>
-
-			<div id="contact-form">
-
-				<h3 id="receive-updates">Receive Updates</h3>
-
-				<!-- Begin MailChimp Signup Form -->
-				<link href="//cdn-images.mailchimp.com/embedcode/slim-081711.css" rel="stylesheet" type="text/css">
-				<style type="text/css">
-				#mc_embed_signup{
-					clear: left;
-					font:14px Helvetica,Arial,sans-serif;
-					/*width:500px;*/
-					/*margin: 0 auto;
-					padding: 10px;*/
-				}
-				#mc-embedded-subscribe-form {
-					padding: 0 !important;
-				}
-				#mce-EMAIL {
-					width: 100% !important;
-				}
-				#mc-embedded-subscribe {
-					width: 100% !important;
-				}
-				/* Add your own MailChimp form style overrides in your site stylesheet or in this style block.
-				We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
-				</style>
-				<div id="mc_embed_signup">
-					<form action="//cash.us12.list-manage.com/subscribe/post?u=343b1cde8705eb7081d468bdd&amp;id=d4d73805a5" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate="">
-						<div id="mc_embed_signup_scroll">
-							<input value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required="" type="email">
-							<!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-							<div style="position: absolute; left: -5000px;" aria-hidden="true"><input name="b_343b1cde8705eb7081d468bdd_d4d73805a5" tabindex="-1" value="" type="text"></div>
-							<div class="clear"><input value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button" type="submit"></div>
-						</div>
-					</form>
-				</div>
-
-				<!--End mc_embed_signup-->
-			</div>
 		</div>
 
 		<div class="footer-bottom-text">
